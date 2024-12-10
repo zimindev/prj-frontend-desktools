8d170a4ad6537155b0016abc90b986fbdd853fbc
Sasha Zimin <zimin.oleksandr@gmail.com>
2024-12-10 20:16:00 +0200

    add comment to snow.js

--- js/winter-time/snow.js
+++ js/winter-time/snow.js
@@ -5,44 +5,44 @@ function createSnowflakes(num) {
  const snowflake = document.createElement('div');
  snowflake.classList.add('snowflake');
 
- // Set random size
- const size = Math.random() * 20 + 10; // size between 10px to 30px
+ // Встановити випадковий розмір
+ const size = Math.random() * 20 + 10; // розмір від 10 пікселів до 30 пікселів
  snowflake.style.fontSize = `${size}px`;
 
- // Set random position
- const leftPosition = Math.random() * 100; // random horizontal position
+ // Встановити випадкову позицію
+ const leftPosition = Math.random() * 100; // випадкове горизонтальне положення
  snowflake.style.left = `${leftPosition}vw`;
 
- // Set random animation duration
- const speed = Math.random() * 10 + 5; // speed between 3s to 8s
+ // Встановити випадкову тривалість анімації
+ const speed = Math.random() * 10 + 5; // швидкість від 3 до 8 секунд
  snowflake.style.animationDuration = `${speed}s`;
 
- // Set random horizontal movement
- const horizontalMovement = (Math.random() - 0.5) * 50; // random horizontal movement
+ // Встановити випадковий горизонтальний рух
+ const horizontalMovement = (Math.random() - 0.5) * 50; // випадковий горизонтальний рух
  snowflake.style.transform = `translateX(${horizontalMovement}px)`;
 
- // Set the snowflake character
+ // Встановіть символ сніжинки
  snowflake.innerHTML = '&#10052;';
 
- // Set random color for the snowflake
- snowflake.style.color = `hsl(${Math.random() * 360}, 100%, 100%)`; // Random color in HSL range
+ // Встановіть випадковий колір для сніжинки
+ snowflake.style.color = `hsl(${Math.random() * 360}, 100%, 100%)`; // Випадковий колір у діапазоні HSL
 
  snowContainer.appendChild(snowflake);
 
- // Remove snowflakes after it falls
+ // Видаліть сніжинки після того, як вони впадуть
  snowflake.addEventListener('animationend', () => {
  snowflake.remove();
  });
  }
 }
 
-// Set the amount of snow
-const amountSnow = 20; // amount of snow
+// Встановити кількість снігу
+const amountSnow = 20; // кількість снігу
 
-// Create snow
+// Створення снігу
 createSnowflakes(amountSnow);
 
-// Create new snow every second
+// Щомиті створюйте новий сніг
 setInterval(() => {
 createSnowflakes(1);
 }, 2000);
